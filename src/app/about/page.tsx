7dc8cc96b3a3f0f419b46d2a1d64e8f471bc0ccf
Tiến Nguyễn Trọng <tiennguyentrong@Tiens-Laptop.local>
2024-03-19 14:07:13 +0700

    footer about 2

--- src/app/about/page.tsx
+++ src/app/about/page.tsx
@@ -112,7 +112,7 @@ export default function Example() {
             </div>
 
             <TedxPic />
-            <ESCPic />
+            {/* <ESCPic /> */}
 
             <div className="bg-white py-24 sm:py-32">
                 <div className="mx-auto grid max-w-7xl gap-x-8 gap-y-20 px-6 lg:px-8 xl:grid-cols-3">
