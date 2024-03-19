d127896c0324e38c2b698aa48e2e75ddfeb660c9
Tiến Nguyễn Trọng <tiennguyentrong@Tiens-Laptop.local>
2024-03-19 14:24:25 +0700

    pple

--- src/app/about/page.tsx
+++ src/app/about/page.tsx
@@ -35,7 +35,7 @@ const people = [
 
 export default function Example() {
     return (
-        <div style={{backgroundColor: 'red'}}>
+        <div>
             <Header />
 
             <div className="relative isolate overflow-hidden bg-gray-900 py-24 sm:py-32">
@@ -44,7 +44,7 @@ export default function Example() {
                     alt="skillcetera"
                     className="absolute inset-0 -z-10 h-full w-full object-cover object-center md:object-center"
                 />
-                {/* <div
+                <div
                     className="hidden sm:absolute sm:-top-10 sm:right-1/2 sm:-z-10 sm:mr-10 sm:block sm:transform-gpu sm:blur-3xl"
                     aria-hidden="true"
                 >
@@ -67,8 +67,9 @@ export default function Example() {
                                 'polygon(74.1% 44.1%, 100% 61.6%, 97.5% 26.9%, 85.5% 0.1%, 80.7% 2%, 72.5% 32.5%, 60.2% 62.4%, 52.4% 68.1%, 47.5% 58.3%, 45.2% 34.5%, 27.5% 76.7%, 0.1% 64.9%, 17.9% 100%, 27.6% 76.8%, 76.1% 97.7%, 74.1% 44.1%)',
                         }}
                     />
-                </div> */}
-                <div className="mx-auto max-w-7xl px-6 lg:px-8">
+                </div>
+
+                {/* <div className="mx-auto max-w-7xl px-6 lg:px-8">
                     <div className="mx-auto max-w-2xl lg:mx-0">
                         <h2 className="text-4xl font-bold tracking-tight text-white sm:text-6xl">Work with us</h2>
                         <p className="mt-6 text-lg leading-8 text-gray-300">
@@ -93,7 +94,7 @@ export default function Example() {
                             ))}
                         </dl>
                     </div>
-                </div>
+                </div> */}
             </div>
 
             <div className="bg-white py-24 sm:py-32">
