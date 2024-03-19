baf8746a958dbcfa856fe3762a1acb2758d39b6b
Tiến Nguyễn Trọng <tiennguyentrong@Tiens-Laptop.local>
2024-03-19 14:20:45 +0700

    oke

--- src/app/about/page.tsx
+++ src/app/about/page.tsx
@@ -35,16 +35,16 @@ const people = [
 
 export default function Example() {
     return (
-        <div>
+        <div style={{backgroundColor: 'red'}}>
             <Header />
 
             <div className="relative isolate overflow-hidden bg-gray-900 py-24 sm:py-32">
                 <img
                     src="/image/about/banner.png"
-                    alt=""
+                    alt="skillcetera"
                     className="absolute inset-0 -z-10 h-full w-full object-cover object-center md:object-center"
                 />
-                <div
+                {/* <div
                     className="hidden sm:absolute sm:-top-10 sm:right-1/2 sm:-z-10 sm:mr-10 sm:block sm:transform-gpu sm:blur-3xl"
                     aria-hidden="true"
                 >
@@ -67,7 +67,7 @@ export default function Example() {
                                 'polygon(74.1% 44.1%, 100% 61.6%, 97.5% 26.9%, 85.5% 0.1%, 80.7% 2%, 72.5% 32.5%, 60.2% 62.4%, 52.4% 68.1%, 47.5% 58.3%, 45.2% 34.5%, 27.5% 76.7%, 0.1% 64.9%, 17.9% 100%, 27.6% 76.8%, 76.1% 97.7%, 74.1% 44.1%)',
                         }}
                     />
-                </div>
+                </div> */}
                 <div className="mx-auto max-w-7xl px-6 lg:px-8">
                     <div className="mx-auto max-w-2xl lg:mx-0">
                         <h2 className="text-4xl font-bold tracking-tight text-white sm:text-6xl">Work with us</h2>
