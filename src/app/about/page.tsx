746acc1415ad654589532f7947bc80db47e1d2e5
Tiến Nguyễn Trọng <tiennguyentrong@Tiens-Laptop.local>
2024-03-19 14:09:26 +0700

    fix about

--- src/app/about/page.tsx
+++ src/app/about/page.tsx
@@ -111,10 +111,10 @@ export default function Example() {
                 </div>
             </div>
 
-            <TedxPic />
+            {/* <TedxPic /> */}
             {/* <ESCPic /> */}
 
-            <div className="bg-white py-24 sm:py-32">
+            {/* <div className="bg-white py-24 sm:py-32">
                 <div className="mx-auto grid max-w-7xl gap-x-8 gap-y-20 px-6 lg:px-8 xl:grid-cols-3">
                     <div className="max-w-2xl">
                         <h2 className="text-3xl font-bold tracking-tight text-gray-900 sm:text-4xl">Meet our leadership</h2>
@@ -137,7 +137,7 @@ export default function Example() {
                         ))}
                     </ul>
                 </div>
-            </div>
+            </div> */}
 
             {/* <div className="relative isolate overflow-hidden bg-gray-900 py-10 sm:py-24 lg:py-12">
                 <div className="mx-auto px-6 lg:px-8">
