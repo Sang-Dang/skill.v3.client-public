a86d48248b334f0e699ff57434e980dcf7579b33
Tiến Nguyễn Trọng <tiennguyentrong@Tiens-Laptop.local>
2024-04-04 20:37:57 +0700

    fix-images

--- src/api.ts
+++ src/api.ts
@@ -1,2 +1,2 @@
-// export const domain = 'https://skira.caucalamdev.io.vn';
-export const domain = 'http://localhost:8080';
+export const domain = 'https://skira.caucalamdev.io.vn';
+// export const domain = 'http://localhost:8080';
