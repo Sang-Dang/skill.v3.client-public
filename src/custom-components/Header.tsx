eaf24ead605593ce4e4bb2a3e3eec99d49bea6a0
Sang Dang <sangdase170287@fpt.edu.vn>
2024-04-10 00:01:33 +0700

    .:

--- src/custom-components/Header.tsx
+++ src/custom-components/Header.tsx
@@ -31,7 +31,7 @@ export default function Header() {
         <header className="bg-white">
             <nav className="mx-auto flex max-w-7xl items-center justify-between p-6 lg:px-8" aria-label="Global">
                 <div className="flex lg:flex-1">
-                    <a href="#" className="-m-1.5 p-1.5">
+                    <Link href="/" className="-m-1.5 p-1.5">
                         <span className="sr-only">Skillcetera</span>
                         <Image
                             src={logo}
@@ -44,7 +44,7 @@ export default function Header() {
                                 userSelect: 'none',
                             }}
                         />
-                    </a>
+                    </Link>
                 </div>
                 <div className="flex lg:hidden">
                     <button
