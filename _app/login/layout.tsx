601ecdd0f653b029e399cdda2709970f78bb04d6
Sang Dang <sangdase170287@fpt.edu.vn>
2024-04-15 11:48:01 +0700

    .

--- _app/login/layout.tsx
+++
@@ -1,5 +0,0 @@
-import { ReactNode, Suspense } from 'react';
-
-export default function layout({ children }: { children: ReactNode }) {
-    return <Suspense fallback={<div>Loading...</div>}>{children}</Suspense>;
-}
